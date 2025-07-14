03890dba1dadfead2db46bd121ddae6d98d841f3
chetanvaswani <chetan.vaswani.cv@gmail.com>
2025-07-14 13:26:58 +0530

    new route added

--- index.ts
+++ index.ts
@@ -6,6 +6,13 @@ app.get('/', (req: Request, res: Response) => {
     res.status(200).send("Hello World this app is crazy CD is running!")
 })
 
+app.get("/cd", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        message: "high on caffine"
+    })
+})
+
 app.listen(3000, () => {
     console.log("app is listening on port 3000.")
 })
